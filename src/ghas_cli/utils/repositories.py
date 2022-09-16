cf9842bf419a0e6958fb9edca531efee0cf6175a
jboursier <jboursier@malwarebytes.com>
2022-09-16 13:07:14 +0200

    Re-arrange code with submodules

---
+++ src/ghas_cli/utils/repositories.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+from typing import List
+import requests
+import json
+
+from . import network
+
+
+def get_org_repositories(status: str, organization: str, token: str) -> List:
+    repositories = []
+    page = 1
+
+    headers = {
+        "accept": "application/vnd.github+json",
+        "authorization": f"Bearer {token}",
+        "User-Agent": "jboursier-mwb/fetch_org_ghas_metrics",
+    }
+    while True:
+        params = {
+            "type": f"{status}",
+            "sort": "full_name",
+            "per_page": 100,
+            "page": page,
+        }
+        repos = requests.get(
+            url=f"https://api.github.com/orgs/{organization}/repos",
+            params=params,
+            headers=headers,
+        )
+        if network.check_rate_limit(repos):
+            break
+
+        if repos.status_code != 200:
+            break
+        for r in repos.json():
+            repositories.append(r["name"])
+
+        if [] == repos.json():
+            break
+        page += 1
+
+    return repositories
---
+++ src/ghas_cli/utils/vulns.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+from typing import List, Any, Dict
+import requests
+import json
+
+from . import network
+
+
+def get_codeql_alerts_repo(
+    repos: str, organization: str, status: str, token: str
+) -> Dict:
+    """Get CodeQL alerts for one or several repositories"""
+
+    headers = {
+        "accept": "application/vnd.github+json",
+        "authorization": f"Bearer {token}",
+        "User-Agent": "jboursier-mwb/fetch_org_ghas_metrics",
+    }
+
+    repositories_alerts = {}
+
+    for repo in repos:
+
+        alerts_repo = []
+        page = 1
+
+        while True:
+
+            params = {"state": "open", "per_page": 100, "page": page}
+            alerts = requests.get(
+                url=f"https://api.github.com/repos/{organization}/{repo}/code-scanning/alerts",
+                params=params,
+                headers=headers,
+            )
+            if network.check_rate_limit(alerts):
+                break
+
+            if alerts.status_code != 200:
+                break
+
+            for a in alerts.json():
+                alerts_repo.append(a)
+
+            if [] == alerts.json():
+                break
+
+            page += 1
+
+        repositories_alerts[repo] = alerts_repo
+
+    return repositories_alerts
