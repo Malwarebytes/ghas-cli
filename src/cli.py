0b0514396ddc05c65050e926f77ec057ea7eb136
jboursier <jboursier@malwarebytes.com>
2022-09-28 19:28:37 +0200

    Bump version to 1.0.0

--- src/cli.py
+++ src/cli.py
@@ -3,7 +3,7 @@
 
 __author__ = "jboursier"
 __copyright__ = "Copyright 2022, Malwarebytes"
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __maintainer__ = "jboursier"
 __email__ = "jboursier@malwarebytes.com"
 __status__ = "Development"
