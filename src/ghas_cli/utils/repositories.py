495f59af1819088d5e4412ed1b786db2113fe295
ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>
2023-05-24 12:41:05 +0100

    small typo
    Signed-off-by: ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>

--- src/ghas_cli/utils/repositories.py
+++ src/ghas_cli/utils/repositories.py
@@ -402,7 +402,7 @@ def load_codeql_base64_template(
 ) -> str:
     with open(f"./templates/codeql-analysis-default.yml", "r") as f:
         data = "".join(f.readlines())
-        data = data.replace("""branches: [ ]""", f"""branches: [{', '.join(f"'branch'" for branch in branches)   }]""")
+        data = data.replace("""branches: [ ]""", f"""branches: [{', '.join(f"'{branch}'" for branch in branches)   }]""")
         data = data.replace("""language: [ ]""", f"""language: {languages}""")
         return base64.b64encode(data.encode("utf-8")).decode("utf-8")
 
