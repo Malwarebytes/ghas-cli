5cac0138f1cfa04d81f6b4bc2602afe74767d5aa
Security & Stuff <74931194+SecurityAndStuff@users.noreply.github.com>
2023-05-26 19:28:17 +0100

    Default filename (#86)
    * use different default filename
    
    Signed-off-by: ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>
    
    * Bump requests from 2.30.0 to 2.31.0
    
    Bumps [requests](https://github.com/psf/requests) from 2.30.0 to 2.31.0.
    - [Release notes](https://github.com/psf/requests/releases)
    - [Changelog](https://github.com/psf/requests/blob/main/HISTORY.md)
    - [Commits](https://github.com/psf/requests/compare/v2.30.0...v2.31.0)
    
    ---
    updated-dependencies:
    - dependency-name: requests
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>
    
    * small typo
    
    Signed-off-by: ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ssousa-mwb <74931194+SecurityAndStuff@users.noreply.github.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Signed-off-by: Joe Nunnelley <joe.nunnelley@tylertech.com>

--- src/ghas_cli/utils/repositories.py
+++ src/ghas_cli/utils/repositories.py
@@ -402,7 +402,7 @@ def load_codeql_base64_template(
 ) -> str:
     with open(f"./templates/codeql-analysis-default.yml", "r") as f:
         data = "".join(f.readlines())
-        data = data.replace("""branches: [ ]""", f"""branches: [{', '.join(f"'branch'" for branch in branches)   }]""")
+        data = data.replace("""branches: [ ]""", f"""branches: [{', '.join(f"'{branch}'" for branch in branches)   }]""")
         data = data.replace("""language: [ ]""", f"""language: {languages}""")
         return base64.b64encode(data.encode("utf-8")).decode("utf-8")
 
@@ -493,7 +493,7 @@ def create_codeql_pr(
         "content": template,
         "branch": target_branch,
         "sha": get_file_sha(
-            organization, repository, headers, ".github/workflows/codeql.yml"
+            organization, repository, headers, ".github/workflows/codeql-analysis-default.yml"
         ),
     }
 
@@ -501,7 +501,7 @@ def create_codeql_pr(
         workflow_commit_payload["message"] = "Update CodeQL analysis workflow"
 
     workflow_commit_resp = network.put(
-        url=f"https://api.github.com/repos/{organization}/{repository}/contents/.github/workflows/codeql.yml",
+        url=f"https://api.github.com/repos/{organization}/{repository}/contents/.github/workflows/codeql-analysis-default.yml",
         headers=headers,
         json=workflow_commit_payload,
     )
