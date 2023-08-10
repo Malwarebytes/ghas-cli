0d6d4690b46c2e98fa522ee6427360a059a485fd
Joe Nunnelley <joe.nunnelley@tylertech.com>
2023-08-10 08:21:11 -0700

    updating messaging and enablement script

--- enable_ghas.sh
+++ enable_ghas.sh
@@ -1,38 +1,54 @@
+#! /usr/bin/env bash
 
+# This script iterates through a list of repos running the ghas-cli commands to enable GHAS on a repo in the tyler-technologies Github org
+# No inputs are required other than the REPOS_LIST file being list of repos one per line in the file.
 
 ORG="tyler-technologies"
-REPO_SLUG=$2
-PUSH_PROTECTION="True"
+REPO_LIST=$1
+IFS=$'\n' read -d '' -r -a REPOS < $REPO_LIST
 
-echo ORG:             $ORG
-echo REPO:            $REPO_SLUG
-#echo TOKEN:           $GITHUB_TOKEN
-#echo PUSH_PROTECTION: $PUSH_PROTECTION
+for i in ${REPOS[@]}; do
+  echo $i
+  REPO_SLUG=$i
+  PUSH_PROTECTION="True"
+  echo ----------------------------------------
+  echo ORG:             $ORG
+  echo REPO:            $REPO_SLUG
+  #echo TOKEN:           $GITHUB_TOKEN
+  #echo PUSH_PROTECTION: $PUSH_PROTECTION
 
-## WORKS
-echo enable actions
-ghas-cli actions set_permissions -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG -e True -a selected
-
-## WORKS
-echo enable dependabot+dependency reviewer
-ghas-cli repositories create_dep_enforcement_pr -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG -b "appsec-ghas-dep-enforcement-pr-${REPO_SLUG}"
-ghas-cli repositories enable_dependabot -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
-ghas-cli issues create -n "About Dependabot" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/dependabot.md
-
-if [ "${PUSH_PROTECTION}" == "True" ]
-then
   ## WORKS
-  echo deploy secret scanner with push proections and create informative issue
-  ghas-cli repositories enable_ss_protection -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
-  ghas-cli issues create -n "About Secret Push Protection" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/secret_scanner_push_protection.md
-  #read varname
-else
-  ## WORKS
-  echo enable secret scanner and create informative issue
-  ghas-cli repositories enable_ss -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
-  ghas-cli issues create -n "About Secret Scanner" -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG  ./templates/secret_scanner.md
-fi
+  echo enable actions
+  ghas-cli actions set_permissions -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG -e True -a all
 
-echo deploy codeql and create an educational issue
-ghas-cli repositories create_codeql_pr -o $ORG -t $GITHUB_TOKEN -b "appsec-ghas-codeql_enable-${REPO_SLUG}" -r $REPO_SLUG
-ghas-cli issues create -n "About Security code scanning" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/codeql.md
+  ## WORKS
+  echo enable dependabot+dependency reviewe
+  ghas-cli repositories create_dep_enforcement_pr -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG -b "appsec-ghas-dep-enforcement-pr-${REPO_SLUG}"
+  ghas-cli repositories enable_dependabot -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
+  ghas-cli issues create -n "About Dependabot" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/dependabot.md
+
+  if [ "${PUSH_PROTECTION}" == "True" ]
+  then
+    ## WORKS
+    echo deploy secret scanner with push proections and create informative issue
+    ghas-cli repositories enable_ss_protection -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
+    ghas-cli issues create -n "About Secret Push Protection" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/secret_scanner_push_protection.md
+    ##read varname
+  else
+    ## WORKS
+    echo enable secret scanner and create informative issue
+    ghas-cli repositories enable_ss -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG
+    ghas-cli issues create -n "About Secret Scanner" -o $ORG -t $GITHUB_TOKEN -r $REPO_SLUG  ./templates/secret_scanner.md
+  fi
+
+  echo deploy codeql and create an educational issue
+  ghas-cli repositories create_codeql_pr -o $ORG -t $GITHUB_TOKEN -b "appsec-ghas-codeql_enable-${REPO_SLUG}" -r $REPO_SLUG
+  ghas-cli issues create -n "About Security code scanning" -r $REPO_SLUG -o $ORG -t $GITHUB_TOKEN ./templates/codeql.md
+
+  read -p 'Proceed (y|n)?' selection
+
+  if [ $selection != 'y' ] && [ $selection != 'Y' ]
+  then
+    exit 0
+  fi
+done
--- templates/codeql_config_create.md
+++ templates/codeql_config_create.md
@@ -4,6 +4,8 @@ We also just opened an informative issue in this repository to give you the cont
 
 However, we encourage you to review the configuration files and ask questions in the Teams `Corp DevOps / Github Community` channel. if you have any questions.
 
+Please note: For this pull request to pass and be mergeable, you will need to update the languages array in the codeql yml file with the languages that your repository is using (eg. python, csharp, ruby, go, etc). Not all languages are supported, so only merge this change if at least one language in the repo is in this [Supported Languages](https://codeql.github.com/docs/codeql-overview/supported-languages-and-frameworks/) list. GitHub is continually adding languages to the support list, so if a language is not supported now it very well may be soon. If your repository does not contain code in one of these supported languages, please do not merge this pull request until there is support. If you merge it without adding a supported language, the pull request checks will fail and you will be unable to merge the pull request.
+
 We are here to help! :thumbsup:
 
 Regards,
