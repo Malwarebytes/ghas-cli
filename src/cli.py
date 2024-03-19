8f3d6b1aa88b8e2b6311ce827970cd889723f254
jboursier <jboursier@malwarebytes.com>
2024-03-19 18:33:03 +0100

    Bump to version 1.6.1
    Signed-off-by: jboursier <jboursier@malwarebytes.com>

--- src/cli.py
+++ src/cli.py
@@ -2,8 +2,8 @@
 #!/usr/bin/env python3
 
 __author__ = "jboursier"
-__copyright__ = "Copyright 2023, Malwarebytes"
-__version__ = "1.5.1"
+__copyright__ = "Copyright 2024, Malwarebytes"
+__version__ = "1.6.1"
 __maintainer__ = "jboursier"
 __email__ = "jboursier@malwarebytes.com"
 __status__ = "Production"
